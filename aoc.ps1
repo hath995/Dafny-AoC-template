fb836daeb8f0d05e40ad57511c955827e4102231
hath995 <hathlesstroubles@gmail.com>
2023-11-06 01:25:07 +0100

    basic powershell wrapper

--- aoc.ps1
+++ aoc.ps1
@@ -1,1 +1,15 @@
-Dafny.exe run --no-verify --unicode-char:false --target:cs "aoc-runner.dfy" --input "libraries/src/FileIO/FileIO.cs" -- "0" "2" "-t"
+param ([Parameter(Mandatory)][int] $problem, [Parameter(Mandatory)][int] $part, [bool] $test)
+if ($test) {
+    $testSwitch = "-t"
+} else {
+    $testSwitch = ""
+}
+$i = 0
+foreach ($cmd in Get-Command Dafny.exe) {
+    $i = $i+1
+}
+if ($i -ne 0) {
+    Dafny.exe run --no-verify --unicode-char:false --target:cs "aoc-runner.dfy" --input "libraries/src/FileIO/FileIO.cs" -- $problem $part $testSwitch
+}else{
+    Write-Output "Dafny.exe is not in terminal path."
+}
