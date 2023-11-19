fc96fda7e4ac0b5f67f7593f8d7fc668498d1b03
Aaron Elligsen <aaron@screenmeet.com>
2023-11-19 22:42:50 +0100

    adding bash script for test runner on mac or linux

--- aoc.sh
+++ aoc.sh
@@ -1,1 +1,28 @@
-dafny run --no-verify --unicode-char:false --target:cs "aoc-runner.dfy" --input "libraries/src/FileIO/FileIO.cs" --
+#!/bin/bash
+usage() {
+cat << EOF
+Usage: 
+  $0 problem part [-t test]
+  Options:
+    problem: The problem number, 1-25
+    part: The problem part, 1 or 2
+    test: optional 
+EOF
+}
+re='^[0-9]+$'
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$#" -lt 2 ] # Request help.
+then
+    usage
+elif ! [[ $1 =~ $re ]] ; then
+       echo "error: problem not a number"
+       usage
+       exit 2
+elif ! [[ $2 =~ $re ]] ; then
+       echo "error: part not a number"
+       usage
+       exit 2
+elif ! [ -x "$(command -v dafny)" ]; then
+    echo "error: Dafny not found in path"
+else
+    dafny run --no-verify --unicode-char:false --target:cs "aoc-runner.dfy" --input "libraries/src/FileIO/FileIO.cs" -- "$1" "$2" "$3"
+fi
