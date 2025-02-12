832be54750159494978acff10ac82da825ee2b15
Aaron Elligsen <hathlesstroubles@gmail.com>
2025-02-12 12:07:12 +0100

    Update aoc.ps1

--- aoc.ps1
+++ aoc.ps1
@@ -9,7 +9,7 @@ foreach ($cmd in Get-Command Dafny.exe) {
     $i = $i+1
 }
 if ($i -ne 0) {
-    Dafny.exe run --no-verify --unicode-char:false --target:cs "aoc-runner.dfy" --standard-libraries -- $problem $part $testSwitch
+    Dafny.exe run --no-verify --unicode-char:true --target:cs "aoc-runner.dfy" --standard-libraries -- $problem $part $testSwitch
 }else{
     Write-Output "Dafny.exe is not in terminal path."
 }
